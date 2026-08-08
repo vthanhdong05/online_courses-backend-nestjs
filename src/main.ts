2893330851962fa6b238e4e0d7f6f2d5e8ad342d
vthanhdong05 <v.thanhdong05@gmail.com>
2026-08-09 00:52:42 +0700

    chore(mongoose): install mongoose & setup environment configuration files

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,18 @@ async function bootstrap() {
     logger: LoggerModule.createLogger(),
   });
 
+  const {
+    PORT = 3000,
+    HOST = 'localhost',
+    APP_PREFIX = '/api',
+    APP_NAME = 'nestjs_app',
+    NODE_ENV = 'development',
+  } = process.env;
+
   initApp(app);
 
-  await app.listen(process.env.PORT ?? 3000);
-
-  Logger.log(`Service is running`);
+  await app.listen(PORT, HOST);
+  const protocol = NODE_ENV === 'production' ? 'https' : 'http';
+  Logger.log(`Service is running at ${protocol}://${HOST}:${PORT}${APP_PREFIX}`, APP_NAME);
 }
 void bootstrap();
