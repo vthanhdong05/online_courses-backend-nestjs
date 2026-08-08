a57e91aa1ea06d34f74879817573ad3f46ee557d
vthanhdong05 <v.thanhdong05@gmail.com>
2026-08-08 23:21:51 +0700

    fix(main): fix import app.Module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './apps/app.module';
 import { initApp } from './init';
 
 async function bootstrap() {
